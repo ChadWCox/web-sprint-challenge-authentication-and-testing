e222dc3d545334ffce2246eeb63e804516bf4f04
Chad Cox <chadcox@Chads-MacBook-Pro.local>
2020-12-17 22:06:38 -0700

    MVP with passing tests

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 const secret = require('../config/secrets.js')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const token = req.headers.authorization
 
   if (!token) {
     res.status(401).json('token required')
   } else {
-    jwt.verify(token, secret, (err, decoded) => {
+    await jwt.verify(token, secret, (err, decoded) => {
       if (err) {
         res.status(401).json('token invalid')
       } else {
